0f019eab89defb27e6d6bb853863f55e6853a0b4
Allan <allan@partyondelivery.com>
2025-10-13 15:33:36 -0500

    Link Mobile Bartenders partner card to new landing page

--- src/app/partners/page.tsx
+++ src/app/partners/page.tsx
@@ -84,7 +84,7 @@ export default function PartnersPage() {
 
   const partnerTypes = [
     {
-      href: '#',
+      href: '/partners/mobile-bartenders',
       icon: (
         <svg className="w-12 h-12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
           <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
